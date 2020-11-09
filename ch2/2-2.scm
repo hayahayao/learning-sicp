ab151a7a9454ab820d644b518b7c22368fc953cb
hayahayao <azurecho13@gmail.com>
2020-11-09 15:28:18 +0800

    📝 ch2.1

--- ch2/2-2.scm
+++ ch2/2-2.scm
@@ -1,3 +1,12 @@
 (define (make-point x y) (cons x y))
-(define (x-point point) ())
-(define (make-segment)
+(define (x-point point) (car point))
+(define (y-point point) (cdr point))
+
+(define (make-segment start end) (cons start end))
+(define (start-segment line) (car line))
+(define (end-segment line) (cdr line))
+
+(define (midpoint-segment line)
+  (make-point 
+    (average (x-point (start-segment line)) (x-point (end-segment line)))
+    (average (y-point (start-segment line)) (y-point (end-segment line)))))
